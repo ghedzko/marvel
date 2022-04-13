a71e6c6f84d9e991aff7e95f66d362684b2f5dfa
Guillermo Hedzko <ghedzko@gmail.com>
2022-04-13 12:23:50 -0300

    style components

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -1,17 +1,29 @@
 import { FunctionComponent } from "react";
+import { MARVEL_AUTH } from "../../constants";
 import { Character as CharacterInterface } from "../../interfaces/Characters";
-
+import { format } from "date-fns";
 import styles from "./Character.module.scss";
 
 interface CharactersProps {
-  key: number;
   character: CharacterInterface;
 }
-const Character: FunctionComponent<CharactersProps> = ({ key, character }) => {
-  console.count("Character number : ");
+const Character: FunctionComponent<CharactersProps> = ({ character }) => {
   return (
-    <div className={styles.wrapper} key={key}>
-      {character.name}
+    <div className={styles.wrapper}>
+      <h3 className={styles.wrapper__title}>{character.name}</h3>
+      <p className={styles.wrapper__text}>{character.description}</p>
+
+      <p className={styles.wrapper__text}>
+        <strong>Character Id: </strong>
+        {character.id}
+      </p>
+      <p className={styles.wrapper__text}>
+        <strong>Last updated: </strong>
+        {format(
+          new Date(character.modified.slice(0, 19)),
+          "dd 'of' MMMM 'of' yyyy"
+        )}
+      </p>
     </div>
   );
 };
